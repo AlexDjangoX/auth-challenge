e12518006bc7e6df1b27239e4234da2c01018c21
AlexDjangoX <alexmonk17@gmail.com>
2022-06-16 18:51:36 +0200

    Updated Schema and Server side to create movie assosiated with a user. One to Many

--- src/server/controllers/movie.js
+++ src/server/controllers/movie.js
@@ -2,8 +2,6 @@ const jwt = require("jsonwebtoken");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-// const jwtSecret = "mysecret";
-
 const getAllMovies = async (req, res) => {
   const movies = await prisma.movie.findMany();
 
@@ -18,13 +16,19 @@ const createMovie = async (req, res) => {
 
     jwt.verify(token, process.env.JWT_SECRET);
 
+    const decoded = jwt.decode(token);
+
     const createdMovie = await prisma.movie.create({
       data: {
         title,
         description,
         runtimeMins,
+        user: {
+          connect: { username: decoded.username },
+        },
       },
     });
+    console.log("33...", createdMovie);
     res.json({ data: createdMovie });
   } catch (error) {
     return res.status(401).json({ error: "Invalid token provided." });
